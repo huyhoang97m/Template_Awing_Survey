78346a4884394543a1403c18cfbd01a48fca645e
Phạm Việt Huy Hoàng <48539618+huyhoang97m@users.noreply.github.com>
2022-05-20 17:38:56 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,107 +1,50 @@
 let stepView = 1;
+let answerOrtherKey = 'lc';
+let answerKey = '\n';
 
-const textFields3 = [
+const arrayQuestions = [
     { type: 'checkbox', ask: 'Cau hoi 1', answer: 'cau tra lpo' },
-    { type: 'radio', ask: 'Cau hoi 2', answer: 'dafc \n between sdasdsasda \n lchtml-worpress' },
+    { type: 'radio', ask: 'Cau hoi 2', answer: 'dafc \n between sdasdsasda \n lchtml-worpress \n cau lcfsg4ws' },
     { type: 'checkbox', ask: 'Cau hoi 3', answer: 'cau 3 \n lcau333' },
     { type: 'radio', ask: 'Cau hoi 4', answer: 'cau 4 \n cau 4.1 \n 4.2' },
     { type: 'radio', ask: 'Cau hoi 5', answer: 'cau ans \n cau fsg4ws \n gsdfvsd' },
-    { type: 'checkbox', ask: 'Cau hoi 6', answer: 'cau ans \n cau lcfsg4ws \n dfvsd' }
+    { type: 'checkbox', ask: 'Cau hoi 6', answer: 'cau ans \n cau lcfsg4ws \n cau lcfsg4ws \n dfvsd ' }
 ];
-
-// var textFields3 = { ten_truong_3 };
-
-// var textAddHtml = '';
-// var arrayAnswer = [];
-// for (let i = 0; i < textFields3.length; i++) {
-//     textAddHtml += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + textFields3[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing' + stepView + '" id="awing-survey-' + i + '">';
-//     indexAnswerOrther = -1;
-//     var tlRadio = textFields3[i].answer;
-//     var myArrayRadio = tlRadio.split("\n");
-//     for (let j = 0; j < myArrayRadio.length; j++) {
-//         if (myArrayRadio[j].includes('lc')) {
-//             var myArrayCheckbox3 = myArrayRadio[j].split("lc");
-//             indexAnswerOrther = j;
-//             textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer" > <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
-//                 '<div id="khac' + i + j + '"' + 'style="margin-left:30px;" class="">' +
-//                 '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA other-text" id="khac' + i + '" required></textarea> </div>';
-
-//         } else {
-
-//             textAddHtml += '<div class="ask d1-flex   mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
-//         }
-
-//     }
-var textAddHtml = '';
-var arrayAnswer = [];
-for (let i = 0; i < textFields3.length; i++) {
-    textAddHtml += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + textFields3[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing" id="awing-survey-' + i + '">';
-    indexAnswerOrther = -1;
-    var tlRadio = textFields3[i].answer;
-    var myArrayRadio = tlRadio.split("\n");
-    for (let j = 0; j < myArrayRadio.length; j++) {
-        if (myArrayRadio[j].includes('lc')) {
-            var myArrayCheckbox3 = myArrayRadio[j].split("lc");
-            indexAnswerOrther = j;
-            textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + j + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
-                '<div id="div-khac' + i + j + '"' + 'style="margin-left:30px;" >' +
-                '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA" id="khac' + i + j + '" required></textarea> </div>';
-        } else {
-
-            textAddHtml += '<div class="ask d1-flex   mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
-        }
-    }
-    textAddHtml += '</form> </div> </li>';
-    arrayAnswer.push({
-        id: i,
-        indexAnswerOrther: indexAnswerOrther,
-        type: textFields3[i].type,
-        valid: false,
-        answer: []
-    })
-}
-$("#q-scroll .list-question").html(textAddHtml);
+let survey = generateSurvey(arrayQuestions);
+$("#q-scroll .list-question").html(survey.html);
 
 function goToHomePage() {
-    var newArrayAnswer = getAnswer(arrayAnswer, ".class-awing");
-    console.log(newArrayAnswer, "newArrayAnswer");
-    // Validation: Tất cả valid trong arrayAnswer = true. => post dữ liệu về
-    // Ngược lại hiện thông báo
+    var newArrayAnswer = getAnswer(survey.arrayAnswer, ".class-awing" + stepView);
+    console.log(newArrayAnswer);
 }
 
-// function getAnswer(arrayAnswer, elementClass) {
-//     $(elementClass).each(function(index) {
-//         var inputName = '.traloi' + index + ':checked';
-//         var newAnswer = $(inputName).length;
-//         if (newAnswer > 0) {
-//             arrayAnswer[index].value = newAnswer;
-
-//             let valid = false;
-//             var question = []
-
-//             $("input[name=question" + index + "]:checked").each(function() {
-//                 question.push($(this).val());
-//             });
-//             arrayAnswer[index].answer = question;
-
-//             let inputNameDiff = '.khac' + index + ':checked';
-//             var newAnswerKhac = $(inputNameDiff).length;
-//             var dien = $('#khac' + index).val();
-
-//             if (newAnswerKhac > 0) {
-//                 if (dien.trim()) {
-//                     valid = true;
-//                     var questionF = $(inputNameDiff).val() + "-" + dien;
-//                     question[arrayAnswer[index].indexAnswerOrther] = questionF;
-//                 }
-//             } else {
-//                 valid = true;
-//             }
-//             arrayAnswer[index].valid = valid;
-//         }
-//     });
-//     return arrayAnswer;
-// }
+function generateSurvey(arrayQuestions) {
+    var html = '';
+    var arrayAnswer = [];
+    for (let i = 0; i < arrayQuestions.length; i++) {
+        html += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + arrayQuestions[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing' + stepView + '" id="awing-survey-' + i + '">';
+        var tlRadio = arrayQuestions[i].answer;
+        var myArrayRadio = tlRadio.split(answerKey);
+        for (let j = 0; j < myArrayRadio.length; j++) {
+            if (myArrayRadio[j].includes(answerOrtherKey)) {
+                var myArrayCheckbox3 = myArrayRadio[j].split(answerOrtherKey);
+                html += '<div class="ask d1-flex mb-1r "> <input id="' + stepView + 'input-question-' + i + j + '" onclick="handleInputOrther(' + i + ',' + j + ')" type="' + arrayQuestions[i].type + '" class="traloi' + i + ' ' + stepView + 'khac' + i + j + '" id="cauhoi' + i + j + '"' + 'name="' + stepView + 'question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
+                    '<div id="' + stepView + 'div-khac' + i + j + '"' + 'style="margin-left:30px;display:none" >' +
+                    '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA" id="' + stepView + 'khac' + i + j + '" required></textarea> </div>';
+            } else {
+                html += '<div class="ask d1-flex   mb-1r "> <input onclick="handleInputOrther(' + i + ',' + j + ')" type="' + arrayQuestions[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="' + stepView + 'question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
+            }
+        }
+        html += '</form> </div> </li>';
+        arrayAnswer.push({
+            id: i,
+            type: arrayQuestions[i].type,
+            valid: false,
+            answer: []
+        })
+    }
+    return { html, arrayAnswer };
+}
 
 function getAnswer(arrayAnswer, elementClass) {
     var newArrayAnswer = arrayAnswer.map((x) => {
@@ -114,27 +57,29 @@ function getAnswer(arrayAnswer, elementClass) {
         var newAnswer = $(inputName).length;
         if (newAnswer > 0) {
             newArrayAnswer[index].value = newAnswer;
-            let valid = false;
             var question = []
-            $("input[name=question" + index + "]").each(function(newIndex) {
+            let valids = [];
+            $("input[name=" + stepView + "question" + index + "]").each(function(newIndex) {
                 if (this.checked) {
-                    let inputNameDiff = '.khac' + index + newIndex + ':checked';
+                    let inputNameDiff = '.' + stepView + 'khac' + index + newIndex + ':checked';
                     var newAnswerKhac = $(inputNameDiff).length;
-                    var dien = $('#khac' + index + newIndex).val();
+                    var dien = $('#' + stepView + 'khac' + index + newIndex).val();
+
                     if (newAnswerKhac > 0) {
                         if (dien.trim()) {
-                            valid = true;
+                            valids.push(true);
                             var questionF = $(inputNameDiff).val() + "-" + dien;
                             question.push(questionF);;
-                        } else valid = false;
+                        } else valids.push(false);
                     } else {
-                        valid = true;
+                        valids.push(true);
                         question.push(this.value);
                     }
+
                 }
             });
             newArrayAnswer[index].answer = question;
-            newArrayAnswer[index].valid = valid;
+            newArrayAnswer[index].valid = !valids.includes(false);
         }
     });
     return newArrayAnswer;
@@ -144,13 +89,35 @@ function getAlphabet(index) {
     return (index + 10).toString(36).toUpperCase();
 }
 
-function showHideTextarea(arrayAnswer) {
-    // for (let i = 0; i < textFields3.length; i++) {
-    //     if (textFields3[i].type = 'checkbox') {
-    //         console.log(textFields3[i].type);
-    //         $("#khac" + i).toggle();
-    //     } else {
-    //         $("#khac" + i).hide();
-    //     }
-    // }
+function handleInputOrther(questionIndex, answerIndex) {
+    if (arrayQuestions[questionIndex].type == 'checkbox') {
+        let inputName = '#' + stepView + 'input-question-' + questionIndex + answerIndex;
+        let arreaName = '#' + stepView + 'khac' + questionIndex + answerIndex;
+        let elementId = '#' + stepView + 'div-khac' + questionIndex + answerIndex;
+        let isShow = $(inputName + ':checkbox:checked').length > 0;
+        showHideDescription(elementId, isShow, arreaName);
+    }
+    if (arrayQuestions[questionIndex].type == 'radio') {
+        arrayQuestions[questionIndex].answer.split(answerKey).map((x, index) => {
+            if (x.includes(answerOrtherKey)) {
+                let inputName = '#' + stepView + 'input-question-' + questionIndex + index;
+                let arreaName = '#' + stepView + 'khac' + questionIndex + index;
+                let elementId = '#' + stepView + 'div-khac' + questionIndex + index;
+                let isShow = $(inputName + ':radio:checked').length > 0;
+                showHideDescription(elementId, isShow, arreaName);
+            }
+        })
+    }
+
+
+
+}
+
+function showHideDescription(elementId, isShow, elementArea) {
+    if (isShow) {
+        $(elementId).show();
+    } else {
+        $(elementArea).val("");
+        $(elementId).hide();
+    }
 }
