d2393065e89758eea53c4851b4ebe19cdf8d2280
Phạm Việt Huy Hoàng <48539618+huyhoang97m@users.noreply.github.com>
2022-05-20 14:06:27 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,10 +11,31 @@ const textFields3 = [
 
 // var textFields3 = { ten_truong_3 };
 
+// var textAddHtml = '';
+// var arrayAnswer = [];
+// for (let i = 0; i < textFields3.length; i++) {
+//     textAddHtml += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + textFields3[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing' + stepView + '" id="awing-survey-' + i + '">';
+//     indexAnswerOrther = -1;
+//     var tlRadio = textFields3[i].answer;
+//     var myArrayRadio = tlRadio.split("\n");
+//     for (let j = 0; j < myArrayRadio.length; j++) {
+//         if (myArrayRadio[j].includes('lc')) {
+//             var myArrayCheckbox3 = myArrayRadio[j].split("lc");
+//             indexAnswerOrther = j;
+//             textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer" > <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
+//                 '<div id="khac' + i + j + '"' + 'style="margin-left:30px;" class="">' +
+//                 '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA other-text" id="khac' + i + '" required></textarea> </div>';
+
+//         } else {
+
+//             textAddHtml += '<div class="ask d1-flex   mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
+//         }
+
+//     }
 var textAddHtml = '';
 var arrayAnswer = [];
 for (let i = 0; i < textFields3.length; i++) {
-    textAddHtml += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + textFields3[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing' + stepView + '" id="awing-survey-' + i + '">';
+    textAddHtml += '<li class="mt-75"> <p class="fw-b lh-15" style="text-align: justify;">' + textFields3[i].ask + '</p> <div class="question2 mt-75"> <form class="q-form class-awing" id="awing-survey-' + i + '">';
     indexAnswerOrther = -1;
     var tlRadio = textFields3[i].answer;
     var myArrayRadio = tlRadio.split("\n");
@@ -22,16 +43,14 @@ for (let i = 0; i < textFields3.length; i++) {
         if (myArrayRadio[j].includes('lc')) {
             var myArrayCheckbox3 = myArrayRadio[j].split("lc");
             indexAnswerOrther = j;
-            textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer" onclick="showHideTextarea()"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
-                '<div id="khac' + i + j + '"' + 'style="margin-left:30px;" class="">' +
-                '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA other-text" id="khac' + i + '" required></textarea> </div>';
-
+            textAddHtml += '<div class="ask d1-flex mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + ' khac' + i + j + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + 'class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:red">' + myArrayCheckbox3[1] + '</p></label></div>' +
+                '<div id="div-khac' + i + j + '"' + 'style="margin-left:30px;" >' +
+                '<textarea placeholder="Vui lòng điền thêm thông tin tại đây" class="text-area textA" id="khac' + i + j + '" required></textarea> </div>';
         } else {
-
             textAddHtml += '<div class="ask d1-flex   mb-1r "> <input type="' + textFields3[i].type + '" class="traloi' + i + '" id="cauhoi' + i + j + '"' + 'name="question' + i + '"' + ' value="' + getAlphabet(j) + '"' + ' class="mt-3px answer"> <label for="cauhoi' + i + j + '" class="textA"> <p style="color:green">' + myArrayRadio[j] + '</p></label></div>';
         }
-
     }
+    textAddHtml += '</form> </div> </li>';
     arrayAnswer.push({
         id: i,
         indexAnswerOrther: indexAnswerOrther,
@@ -39,95 +58,103 @@ for (let i = 0; i < textFields3.length; i++) {
         valid: false,
         answer: []
     })
-    textAddHtml += '</form> </div> </li>';
 }
 // console.log(textAddHtml);
 $("#q-scroll .list-question").html(textAddHtml);
 
-// var newtextFields3 = textFields3.map(changeField);
-
-// function changeField(textFields3) {
-//     return {
-//         type: textFields3.type,
-//         cauhoi: textFields3.ask,
-//         traloi: textFields3.answer.split("\n"),
-//     };
-// };
 
 
 // console.log(arrayAnswer);
 
 function goToHomePage() {
-    // var valuecc = [];
-
-    // $('.q-form').each(function(index) {
-    //     // console.log($(this));
-    //     console.log(index);
-    //     // valuecc.push($(this).val());
-    // });
-    // console.log(valuecc);
-
-    arrayAnswer = getAnswer(arrayAnswer, ".class-awing" + stepView);
-    console.log(arrayAnswer);
-
+    var newArrayAnswer = getAnswer(arrayAnswer, ".class-awing");
+    console.log(newArrayAnswer, "newArrayAnswer");
     // Validation: Tất cả valid trong arrayAnswer = true. => post dữ liệu về
     // Ngược lại hiện thông báo
 }
 
+// function getAnswer(arrayAnswer, elementClass) {
+//     $(elementClass).each(function(index) {
+//         var inputName = '.traloi' + index + ':checked';
+//         var newAnswer = $(inputName).length;
+//         if (newAnswer > 0) {
+//             arrayAnswer[index].value = newAnswer;
+
+//             let valid = false;
+//             var question = []
+
+//             $("input[name=question" + index + "]:checked").each(function() {
+//                 question.push($(this).val());
+//             });
+//             arrayAnswer[index].answer = question;
+
+//             let inputNameDiff = '.khac' + index + ':checked';
+//             var newAnswerKhac = $(inputNameDiff).length;
+//             var dien = $('#khac' + index).val();
+
+//             if (newAnswerKhac > 0) {
+//                 if (dien.trim()) {
+//                     valid = true;
+//                     var questionF = $(inputNameDiff).val() + "-" + dien;
+//                     question[arrayAnswer[index].indexAnswerOrther] = questionF;
+//                 }
+//             } else {
+//                 valid = true;
+//             }
+//             arrayAnswer[index].valid = valid;
+//         }
+//     });
+//     return arrayAnswer;
+// }
+
 function getAnswer(arrayAnswer, elementClass) {
+    var newArrayAnswer = arrayAnswer.map((x) => {
+        x.valid = false;
+        x.answer = [];
+        return x;
+    })
     $(elementClass).each(function(index) {
         var inputName = '.traloi' + index + ':checked';
         var newAnswer = $(inputName).length;
-        // if (newAnswer > 0) {
-        //     arrayAnswer[index].value = newAnswer;
-        //     arrayAnswer[index].valid = true;
-        //     var question = []
-        //     $("input[name=question" + index + "]:checked").each(function() {
-        //         question.push($(this).val());
-        //     });
-        //     arrayAnswer[index].answer = question;
-        // }
         if (newAnswer > 0) {
-            arrayAnswer[index].value = newAnswer;
-
+            newArrayAnswer[index].value = newAnswer;
             let valid = false;
             var question = []
-
-            $("input[name=question" + index + "]:checked").each(function() {
-                question.push($(this).val());
-            });
-            arrayAnswer[index].answer = question;
-
-            let inputNameDiff = '.khac' + index + ':checked';
-            var newAnswerKhac = $(inputNameDiff).length;
-            var dien = $('#khac' + index).val();
-
-            if (newAnswerKhac > 0) {
-                if (dien.trim()) {
-                    valid = true;
-                    var questionF = $(inputNameDiff).val() + "-" + dien;
-                    question[arrayAnswer[index].indexAnswerOrther] = questionF;
+            $("input[name=question" + index + "]").each(function(newIndex) {
+                if (this.checked) {
+                    let inputNameDiff = '.khac' + index + newIndex + ':checked';
+                    var newAnswerKhac = $(inputNameDiff).length;
+                    var dien = $('#khac' + index + newIndex).val();
+                    if (newAnswerKhac > 0) {
+                        if (dien.trim()) {
+                            valid = true;
+                            var questionF = $(inputNameDiff).val() + "-" + dien;
+                            question.push(questionF);;
+                        } else valid = false;
+                    } else {
+                        valid = true;
+                        question.push(this.value);
+                    }
                 }
-            } else {
-                valid = true;
-            }
-            arrayAnswer[index].valid = valid;
+            });
+            newArrayAnswer[index].answer = question;
+            newArrayAnswer[index].valid = valid;
         }
     });
-    return arrayAnswer;
+    return newArrayAnswer;
 }
 
 function getAlphabet(index) {
     return (index + 10).toString(36).toUpperCase();
 }
 
-function showHideTextarea() {
-    for (let i = 0; i < textFields3.length; i++) {
-        if (textFields3[i].type = 'checkbox') {
-            console.log(textFields3[i].type);
-            $("#khac" + i).toggle();
-        } else {
-            $("#khac" + i).hide();
-        }
-    }
+function showHideTextarea(arrayAnswer) {
+    // for (let i = 0; i < textFields3.length; i++) {
+    //     if (textFields3[i].type = 'checkbox') {
+    //         console.log(textFields3[i].type);
+    //         $("#khac" + i).toggle();
+    //     } else {
+    //         $("#khac" + i).hide();
+    //     }
+    // }
 }
